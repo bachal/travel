41795d2b7947159b43a50bd7f457bd835479fd15
somnathbachal023@gmail.com <somnathbachal023@gmail.com>
2020-09-07 11:07:52 +0530

    change img src

---
+++ build/precache-manifest.e2686ad7b61df1835b2f4f3d46c0b88b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aeb55d686ce9e9efc88ecc08ff311cd",
+    "url": "/travel/index.html"
+  },
+  {
+    "revision": "0f432026e3a56aa7d498",
+    "url": "/travel/static/css/2.db6f3515.chunk.css"
+  },
+  {
+    "revision": "498bb3807f8c2791c09f",
+    "url": "/travel/static/css/main.6c8554a5.chunk.css"
+  },
+  {
+    "revision": "0f432026e3a56aa7d498",
+    "url": "/travel/static/js/2.a08466ba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/travel/static/js/2.a08466ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498bb3807f8c2791c09f",
+    "url": "/travel/static/js/main.fcce0434.chunk.js"
+  },
+  {
+    "revision": "f7109eb5c895039947e0",
+    "url": "/travel/static/js/runtime-main.b69cbd31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travel/static/media/logo.5d5d9eef.svg"
+  }
+]);
